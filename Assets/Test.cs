6e71b8ef070e3e6b61b2a167bc7a11a5c8542bff
Hearts96 <71250257+Hearts96@users.noreply.github.com>
2020-09-27 20:34:44 +0900

    ScriptTestの課題

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+
+        int[] array = new int[5];
+        array[0] = 40;
+        array[1] = 30;
+        array[2] = 50;
+        array[3] = 20;
+        array[4] = 10;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
